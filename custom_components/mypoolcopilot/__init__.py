93ad680c81c78b3511d3957adf3c443ea773c51b
gstax <stax@ik.me>
2025-05-07 21:14:21 +0200

    fix: full support for config_flow with token_entity fallback (v4.6.8)

--- custom_components/mypoolcopilot/__init__.py
+++ custom_components/mypoolcopilot/__init__.py
@@ -3,6 +3,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
 from .coordinator import PoolCopilotDataUpdateCoordinator
@@ -25,7 +26,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_config_entry_first_refresh()
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
